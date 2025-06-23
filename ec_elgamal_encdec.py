59bf52f54b67ce08c6f429d560722937ed8389d5
maton369 <ysk901085@gmail.com>
2025-06-23 09:11:22 +0900

    Add: 楕円曲線Elgamalのコードのコメントを追加

--- ec_elgamal_encdec.py
+++ ec_elgamal_encdec.py
@@ -7,6 +7,7 @@ a = -1
 b = 2
 p = 37
 
+# a, p を固定して部分適用した scalar_multiply を作成
 scalar_multiply = partial(scalar_multiply, a=a, p=p)
 
 # 基準点の座標（公開パラメータ）
@@ -15,11 +16,13 @@ G = (25, 5)
 n = 47
 
 
+# ECDH（楕円曲線 Diffie-Hellman）共有鍵生成
 def ecdh(public_key, private_key):
     shared_secret = scalar_multiply(public_key, private_key)
     return shared_secret
 
 
+# ElGamal暗号（楕円曲線）による暗号化関数
 def elgamal_enc(message, alice_private_key, bob_public_key):
     # M + abG の通常のベクトル和を計算
     m_x, m_y = message
@@ -28,6 +31,7 @@ def elgamal_enc(message, alice_private_key, bob_public_key):
     return cipher
 
 
+# ElGamal暗号（楕円曲線）による復号関数
 def elgamal_dec(cipher, alice_public_key, bob_private_key):
     # M + abG - abG を計算して、平文に戻す
     c_x, c_y = cipher
@@ -35,20 +39,26 @@ def elgamal_dec(cipher, alice_public_key, bob_private_key):
     return ((c_x - abG_x) % p, (c_y - abG_y) % p)
 
 
+# アリスの鍵ペア生成
 alice_private_key = 9
 alice_public_key = aG = scalar_multiply(G, alice_private_key)
 print(f"alice_private_key = {alice_private_key}, alice_public_key = {alice_public_key}")
 
+# ボブの鍵ペア生成
 bob_private_key = 23
 bob_public_key = bG = scalar_multiply(G, bob_private_key)
 print(f"bob_private_key = {bob_private_key}, bob_public_key = {bob_public_key}")
 
 print()
 
+# 暗号化対象メッセージ（楕円曲線上の点と仮定）
 message = (5, 10)
 print(f"message = {message}")
+
+# 暗号化処理
 cipher = elgamal_enc(message, alice_private_key, bob_public_key)
 print(f"cipher = {cipher}, alice_public_key = {alice_public_key}")
 
+# 復号処理
 plain_text = elgamal_dec(cipher, alice_public_key, bob_private_key)
 print(f"plain_text = {plain_text}")
